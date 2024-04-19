a95c366cbf9a8641df13e171e0ecd6bae97a42f9
Aman <developer@codelinear.com>
2024-04-19 10:33:11 +0530

    about page and offer change

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,6 +1,6 @@
 import About from "@/components/Aboutpage/About";
 import Mission from "@/components/Aboutpage/Mission";
-import Offer from "@/components/Aboutpage/offer";
+import Offer from "@/components/Aboutpage/Offer";
 import Team from "@/components/Aboutpage/Team";
 import React from "react";
 
